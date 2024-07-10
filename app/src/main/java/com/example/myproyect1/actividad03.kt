50d277073b1bee4230aa5f77a8d7b6af11519737
Magaguilar1234 <magaguilargn@gmail.com>
2024-07-10 15:56:00 -0600

    Segundo commit

---
+++ app/src/main/java/com/example/myproyect1/actividad03.kt
@@ -0,0 +1,12 @@
+package com.example.myproyect1
+
+fun main() {
+
+    val peso = 68.0  // Peso en kilogramos
+    val altura = 1.69f  // Altura en metros
+
+// Cálculo del IMC
+    val imc = (peso / (altura * altura))
+
+    println("tu masa muscular es "+ imc)
+    }
